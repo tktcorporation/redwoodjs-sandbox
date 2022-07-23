cac1068243f75bc2a810849a4cf28d1790b0ce57
tkt <37575408+tktcorporation@users.noreply.github.com>
2022-07-23 18:20:38 +0000

    add Contact page

--- redwoodblog/web/src/Routes.tsx
+++ redwoodblog/web/src/Routes.tsx
@@ -24,6 +24,7 @@ const Routes = () => {
       </Set>
       <Set wrap={BlogLayout}>
         <Route path="/article/{id:Int}" page={ArticlePage} name="article" />
+        <Route path="/contact" page={ContactPage} name="contact" />
         <Route path="/about" page={AboutPage} name="about" />
         <Route path="/" page={HomePage} name="home" />
       </Set>
