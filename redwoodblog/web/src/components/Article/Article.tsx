8e67d0a9a3b15763ce3a3b41348968021d0f993f
tkt <37575408+tktcorporation@users.noreply.github.com>
2022-07-25 16:09:31 +0000

    bring tutrial styles up https://github.com/redwoodjs/redwood-tutorial

--- redwoodblog/web/src/components/Article/Article.tsx
+++ redwoodblog/web/src/components/Article/Article.tsx
@@ -10,12 +10,11 @@ const Article = ({ article }: Props) => {
   return (
     <article>
       <header>
-        <h2>
+        <h2 className="text-xl text-blue-700 font-semibold">
           <Link to={routes.article({ id: article.id })}>{article.title}</Link>
         </h2>
       </header>
-      <div>{article.body}</div>
-      <div>Posted at: {article.createdAt}</div>
+      <div className="mt-2 text-gray-900 font-light">{article.body}</div>
     </article>
   )
 }
--- redwoodblog/web/src/pages/LoginPage/LoginPage.tsx
+++ redwoodblog/web/src/pages/LoginPage/LoginPage.tsx
@@ -44,7 +44,7 @@ const LoginPage = () => {
     <>
       <MetaTags title="Login" />
 
-      <main className="rw-main">
+      <main className="rw-main w-96 mx-auto mt-12">
         <Toaster toastOptions={{ className: 'rw-toast', duration: 6000 }} />
         <div className="rw-scaffold rw-login-container">
           <div className="rw-segment">
@@ -100,7 +100,7 @@ const LoginPage = () => {
                   <div className="rw-forgot-link">
                     <Link
                       to={routes.forgotPassword()}
-                      className="rw-forgot-link"
+                      className="rw-forgot-link underline"
                     >
                       Forgot Password?
                     </Link>
@@ -115,7 +115,7 @@ const LoginPage = () => {
               </div>
             </div>
           </div>
-          <div className="rw-login-link">
+          <div className="rw-login-link mt-2 text-center">
             <span>Don&apos;t have an account?</span>{' '}
             <Link to={routes.signup()} className="rw-link">
               Sign up!
